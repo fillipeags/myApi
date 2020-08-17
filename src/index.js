fa18ef57d62c31c1e453e07d495faf577cabb961
Fillipe <fillipe484@gmail.com>
2020-08-17 14:27:38 -0300

    eslint config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const { response } = require('express');
 
 const app = express();
 
-app.get('/', (request,response) => {
-  response.send('Hello World!');
+app.get('/', (request, response) => {
+  response.send('Hello World!!');
 });
 
 app.listen(3000, () => console.log('🔥 Server is running at http://localhost:3000'));
