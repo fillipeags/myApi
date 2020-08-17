e390c6892f559382a0c80c7e4badce90184699bd
Fillipe <fillipe484@gmail.com>
2020-08-17 14:50:51 -0300

    Creating routes and contactController

---
+++ src/App/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index(request, response) {
+    response.send('Hello World ');
+  }
+
+  show() {
+
+  }
+
+  store() {
+
+  }
+
+  update() {
+
+  }
+
+  delete() {
+
+  }
+}
+
+module.exports = new ContactController();
