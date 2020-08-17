6c230a8e213e2166e5ec2fc489fa99a87ffb93e6
Fillipe <fillipe484@gmail.com>
2020-08-17 16:41:37 -0300

    create Route

--- src/App/controllers/ContactController.js
+++ src/App/controllers/ContactController.js
@@ -19,6 +19,31 @@ class ContactController {
     response.json(contact);
   }
 
+  async store(request, response) {
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This email has already been taken' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+    response.json(contact);
+  }
+
+  update() {
+
+  }
+
   async delete(request, response) {
     const { id } = request.params;
 
@@ -31,14 +56,6 @@ class ContactController {
 
     // 204 - No Content
     response.sendStatus(204);
-  }
-
-  store() {
-
-  }
-
-  update() {
-
   }
 }
 
