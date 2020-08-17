153bf02c6dd50b220b68b0890451abcb1d98f30a
Fillipe <fillipe484@gmail.com>
2020-08-17 14:09:49 -0300

    initializing node server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
-const routes = require('./routes');
+const { response } = require('express');
 
 const app = express();
 
-app.use(express.json());
-app.use(routes);
+app.get('/', (request,response) => {
+  response.send('Hello World!');
+});
 
-app.listen(3000, () => console.log('🔥 Server started running at http://localhost:3000'));
+app.listen(3000, () => console.log('🔥 Server is running at http://localhost:3000'));
